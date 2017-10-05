2dfbf9b7404cc08ec4851f7be8fd24aefe70e732
BeeBee <beetimberalke2@gmail.com>
2017-10-05 22:46:05 +0700

    ch

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var StarsModule = require('./dist/stars.js');
-var HalfStarsModule = require('./dist/stars-half.js');
-module.exports=StarsModule
+var Stars = require('./dist/stars.js');
+var HalfStars = require('./dist/stars-half.js');
+module.exports=HalfStars
